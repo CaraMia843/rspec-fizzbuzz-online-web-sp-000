cf8a7bd4ca18eb4246d94902c62581e2c5323014
Cara Zellner <carajeanine@gmail.com>
2020-10-06 17:51:34 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,14 +2,7 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(num)
-  if num % 3 == 0
-    "Fizz"
-  elsif num % 5 == 0 
-    "Buzz"
-  elsif num % 15 == 0
-    "Fizzbuzz"
-  else
-    nil
+  if 
   end
 end
 
