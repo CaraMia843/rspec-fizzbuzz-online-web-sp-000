7e85823f62c9d403597db6d1e4ddcd3dc87e5370
Cara Zellner <carajeanine@gmail.com>
2020-10-06 17:41:26 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,14 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
-def fizzbuzz
+def fizzbuzz(num)
   
+end
+
+def fizz(num)
+  
+end
+
+def buzz(num)
+
 end
